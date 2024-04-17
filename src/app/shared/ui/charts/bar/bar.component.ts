1afe2c52b55ac2652a7f59e4503b465d35c2a61b
adrian <vreyes.com>
2024-04-17 00:46:03 -0600

    custom bar charts

--- src/app/shared/ui/charts/bar/bar.component.ts
+++ src/app/shared/ui/charts/bar/bar.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { ChartComponent } from '@progress/kendo-angular-charts';
+import { ChartComponent, SeriesVisualArgs } from '@progress/kendo-angular-charts';
+import { Element, Circle, Path, Group, geometry} from '@progress/kendo-drawing';
 import { Componente } from 'src/app/models/bi';
 import { ComponenteModel } from 'src/app/models/seccion.data';
 import { ENUM_TIPO_DE_COMPONETES } from 'src/app/shared/enums/enums';
+const { transform, Circle: GeomCircle } = geometry;
 
 @Component({
   selector: 'app-bar',
@@ -40,4 +42,51 @@ export class BarComponent {
       this.eventImage.emit(dataURI)
     });
   }
+
+  public radiusEnd = (e: SeriesVisualArgs) => {
+    const group = new Group();
+    const barSize = -0;
+    // var visual: Element = e.createVisual();
+
+    // visual.transform(
+    //   geometry
+    //     .transform()
+    //     .scale(1, barSize / e.rect.size.height, e.rect.center())
+    // );
+    // group.append(visual)
+    var height = e.rect.size.height + barSize;
+    var width = e.rect.size.width;
+    var radius = height / 2;
+    var originY = e.rect.origin.y;
+    var originX = e.rect.origin.x;
+    var pointX = originX + width - radius;
+    var pointY = originY + radius;
+
+    const _geometry = new GeomCircle([pointX, pointY], radius);
+    const circle = new Circle(_geometry, {
+      stroke: { color: e.series.color },
+      fill: { color: e.series.color }
+    });
+    
+
+    const path = new Path({
+      stroke: { color: e.series.color },
+      fill: { color: e.series.color }
+    });
+    path
+      .moveTo(originX, originY)
+      .lineTo(pointX, originY)
+      .lineTo(pointX, originY + height)
+      .lineTo(originX, originY + height)
+      .close();
+
+   
+    group.append(circle, path);
+    
+
+    //return visual;
+    //group.append()
+
+    return group;
+  };
 }
