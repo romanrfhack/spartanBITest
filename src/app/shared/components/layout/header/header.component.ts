229226f5f44a9d4bcaa471f2bedc7b9f9afd8cce
romanrfhack <romanrfhack@gmail.com>
2024-07-11 16:24:32 -0600

    Quitar valores seleccionados

--- src/app/shared/components/layout/header/header.component.ts
+++ src/app/shared/components/layout/header/header.component.ts
@@ -6,11 +6,7 @@ import { take } from 'rxjs';
 import { Componente, SeccionesData } from 'src/app/models/bi';
 import { environment } from 'src/app/environments/environment';
 import * as _ from 'lodash';
-import {
-  ComponenteModel,
-  SeccionModel,
-  SeccionesDataModel,
-} from 'src/app/models/seccion.data';
+import { ComponenteModel, SeccionModel, SeccionesDataModel } from 'src/app/models/seccion.data';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
@@ -59,8 +55,7 @@ export class HeaderComponent implements OnInit {
       next: (res) => {
         // console.log(res);
       },
-    });
-    //esperar 3 segundos y ejecutar      this.onSearch()
+    });    
     setTimeout(() => {
       this.onSearch();
     }, 2000);
@@ -76,44 +71,61 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+  // private _builtSeccionFilters(metadata: SeccionesDataModel) {
+  //   const _meta = metadata.secciones.find(
+  //     (m: SeccionModel) => m.id == 2 || m.tipo == 'filters'
+  //   );
+  //   if (_meta) {
+  //     this.seccionFilters = _.cloneDeep(_meta);
+
+  //     this.seccionFilters.items.map((e: Componente) => {
+
+  //       let isArr: boolean = false;
+  //       let valueSelected: string | number | boolean | string[] | null;
+  //       let counValuesSelected: number = 0;
+
+  //       (e.values as Value[]).map((value) => {
+  //         if (value.selected) {
+  //           counValuesSelected++;
+  //         }
+  //       });
+
+  //       if (counValuesSelected > 1) {
+  //         isArr = true;
+  //         valueSelected = [];
+  //       }
+
+  //       (e.values as Value[]).map((value) => {
+  //         if (value.selected) {
+  //           if (isArr) {
+  //             (valueSelected as string[]).push(value.value);
+  //           } else {
+  //             valueSelected = value.value;
+  //           }
+  //         }
+  //       });
+  //       this.form.setControl(e.filterName ?? 'null', new FormControl(valueSelected));
+
+  //     });
+  //   }
+  // }
+
   private _builtSeccionFilters(metadata: SeccionesDataModel) {
     const _meta = metadata.secciones.find(
       (m: SeccionModel) => m.id == 2 || m.tipo == 'filters'
     );
     if (_meta) {
       this.seccionFilters = _.cloneDeep(_meta);
-
+  
       this.seccionFilters.items.map((e: Componente) => {
-
-        let isArr: boolean = false;
-        let valueSelected: string | number | boolean | string[] | null;
-        let counValuesSelected: number = 0;
-
-        (e.values as Value[]).map((value) => {
-          if (value.selected) {
-            counValuesSelected++;
-          }
-        });
-
-        if (counValuesSelected > 1) {
-          isArr = true;
-          valueSelected = [];
-        }
-
-        (e.values as Value[]).map((value) => {
-          if (value.selected) {
-            if (isArr) {
-              (valueSelected as string[]).push(value.value);
-            } else {
-              valueSelected = value.value;
-            }
-          }
-        });
-
+        let valueSelected: string | number | boolean | string[] | null = null;
+  
+        // Aquí, inicializamos el valor seleccionado como null para no tener nada preseleccionado
         this.form.setControl(e.filterName ?? 'null', new FormControl(valueSelected));
       });
     }
   }
+  
 
   private _getItemDefaultSelected() {
     this.seccion!.items.forEach((componente: Componente) => {
@@ -131,22 +143,27 @@ export class HeaderComponent implements OnInit {
     this.onChangeTipoVista.emit(this.selectedItem);
   }
 
+  removeNullProperties(obj) {
+    return Object.fromEntries(Object.entries(obj).filter(([_, value]) => value !== null));
+  }
+
   onSearch() {
     console.log(`filtrando...`);
-    const obj: object = this.form.value;    
+    const obj: object = this.form.value;
     for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        // Verificamos si la propiedad es un arreglo
-        if (Array.isArray(obj[key])) {
-          // Convertimos el arreglo a un string, separando los elementos con comas
+      if (obj.hasOwnProperty(key)) {        
+        if (Array.isArray(obj[key])) {          
           obj[key] = obj[key].join('|');
         }
       }
     }
-        
-    this.blockUI.start();    
+
+    this.blockUI.start();
+
+    const cleanedFormValue = this.removeNullProperties(this.form.value);
+
     this._dashboardService
-      .getSectionBodyDashboard(JSON.stringify(this.form.value))
+      .getSectionBodyDashboard(JSON.stringify(cleanedFormValue))
       .pipe(take(1))
       .subscribe(
         (data: SeccionesData) => {
@@ -158,28 +175,6 @@ export class HeaderComponent implements OnInit {
         }
       );
   }
-
-  // onChangeTabGroup($event: any, filter: ComponenteModel) {
-  //   console.log(`onChangeTabGroup => $event`, $event)
-  //   console.log(`onChangeTabGroup => filter`, filter)
-  //   this.queryParams = { ...this.queryParams, [filter.filterName]: $event.value }
-  //   console.log(`this.queryParams`, this.queryParams)
-  // }
-
-  // onSelectChange($event: any, filter: ComponenteModel) {
-  //   console.log(`onSelectChange => $event`, $event)
-  //   console.log(`onSelectChange => filter`, filter)
-  //   this._checkSelectAll($event, filter)
-  //   this.queryParams = { ...this.queryParams, [filter.filterName]: $event.value.join() }
-  //   console.log(`onSelectChange`, this.queryParams)
-  // }
-
-  // onCheckBoxChange($event: any, filter: any) {
-  //   console.log(`onCheckBoxChange => $event`, $event)
-  //   console.log(`onCheckBoxChange => filter`, filter)
-  //   this.queryParams = { ...this.queryParams, [filter.filterName]: $event.checked }
-  //   console.log(`onCheckBoxChange`, this.queryParams)
-  // }
 
   onLimpiarFiltros() {
     this.blockUI.start();
@@ -235,25 +230,6 @@ export class HeaderComponent implements OnInit {
   public get ENUM_TIPO_DE_COMPONETES(): typeof ENUM_TIPO_DE_COMPONETES {
     return ENUM_TIPO_DE_COMPONETES;
   }
-
-  // private _checkSelectAll(itemSelected, filter: ComponenteModel) {
-  //   console.log(`itemSelected`, itemSelected)
-  //   if (filter.nombreComponente == ENUM_TIPO_DE_COMPONETES.MULTI_SELECT) {
-  //     if (itemSelected.value[0] == null) {
-  //       for (const item of filter.values) {
-  //         item.selected = true
-  //       }
-  //       const indexComponente = this.seccionFilters.items.findIndex((c:Componente) =>  c.filterName == filter.filterName)
-  //       if(indexComponente != -1){
-  //         this.seccionFilters.items[indexComponente].values= filter.values
-  //       }
-  //       console.log(this.seccionFilters)
-  //     } else {
-  //       //filter.values = filter.values.filter((item:any) => item.id != n)
-  //     }
-  //   }
-
-  // }
 
   misDashboards() {
     this._router.navigate(['/dashboard']);
